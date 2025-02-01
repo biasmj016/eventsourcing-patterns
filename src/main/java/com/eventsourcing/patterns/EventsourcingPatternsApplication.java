49dde66f548d2da50fb59ccc1e6e9c465b6b3c0a
biasmj016 <mjkim031@gmail.com>
2025-02-01 21:34:23 +0900

    init project

---
+++ src/main/java/com/eventsourcing/patterns/EventsourcingPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.eventsourcing.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsourcingPatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventsourcingPatternsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eventsourcing/patterns/EventsourcingPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventsourcing.patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsourcingPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
