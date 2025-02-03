3009039d5852cbe392fd687e83dca4cfbf891259
biasmj016 <mjkim031@gmail.com>
2025-02-03 13:39:11 +0900

    rename class

--- src/main/java/com/eventsourcing/payment/PaymentApplication.java
+++ src/main/java/com/eventsourcing/payment/PaymentApplication.java
@@ -1,13 +1,13 @@
-package com.eventsourcing.patterns;
+package com.eventsourcing.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EventsourcingPatternsApplication {
+public class PaymentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EventsourcingPatternsApplication.class, args);
+        SpringApplication.run(PaymentApplication.class, args);
     }
 
 }
--- src/test/java/com/eventsourcing/payment/PaymentApplicationTests.java
+++ src/test/java/com/eventsourcing/payment/PaymentApplicationTests.java
@@ -1,10 +1,10 @@
-package com.eventsourcing.patterns;
+package com.eventsourcing.payment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EventsourcingPatternsApplicationTests {
+class PaymentApplicationTests {
 
     @Test
     void contextLoads() {
