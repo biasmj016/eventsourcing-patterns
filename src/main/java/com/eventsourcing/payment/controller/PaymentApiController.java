b0374117a8a6f1c51d91bd05f46a1ade2188a7e7
biasmj016 <mjkim031@gmail.com>
2025-02-10 15:20:13 +0900

    공통 response 컨트롤러 적용

--- src/main/java/com/eventsourcing/payment/controller/PaymentApiController.java
+++ src/main/java/com/eventsourcing/payment/controller/PaymentApiController.java
@@ -1,5 +1,6 @@
 package com.eventsourcing.payment.controller;
 
+import com.eventsourcing.payment.config.response.ApiResponse;
 import com.eventsourcing.payment.controller.request.PaymentRequest;
 import com.eventsourcing.payment.controller.request.PaymentVerifyRequest;
 import com.eventsourcing.payment.service.command.PaymentCommandService;
@@ -7,10 +8,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.CompletableFuture;
 
+import static com.eventsourcing.payment.config.response.ApiResponse.success;
+
 @RestController
 @RequestMapping("/payments")
 public class PaymentApiController {
-
     private final PaymentCommandService commandService;
 
     public PaymentApiController(PaymentCommandService commandService) {
@@ -18,17 +20,20 @@ public class PaymentApiController {
     }
 
     @PostMapping("/request")
-    public CompletableFuture<Object> requestPayment(@RequestBody PaymentRequest request) {
-        return commandService.requestPayment(request.paymentId(), request.memberId(), request.itemId(), request.count());
+    public CompletableFuture<ApiResponse<Object>> requestPayment(@RequestBody PaymentRequest request) {
+        return commandService.requestPayment(request.paymentId(), request.memberId(), request.itemId(), request.count())
+                .thenApply(result -> success());
     }
 
     @PostMapping("/verify")
-    public CompletableFuture<Object> verify(@RequestBody PaymentVerifyRequest request) {
-        return commandService.verifyPayment(request.paymentId(), request.memberId(), request.itemId(), request.amount());
+    public CompletableFuture<ApiResponse<Object>> verify(@RequestBody PaymentVerifyRequest request) {
+        return commandService.verifyPayment(request.paymentId(), request.memberId(), request.itemId(), request.amount())
+                .thenApply(result -> success());
     }
 
     @PostMapping("/approve/{paymentId}")
-    public CompletableFuture<Object> approvePayment(@PathVariable String paymentId) {
-        return commandService.approvePayment(paymentId);
+    public CompletableFuture<ApiResponse<Object>> approvePayment(@PathVariable String paymentId) {
+        return commandService.approvePayment(paymentId)
+                .thenApply(result -> success());
     }
 }
